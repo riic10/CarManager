0e1fc0accdd6165261294f800198646b187e2210
Eric Ko <koeric@students.cs.ubc.ca>
2025-07-10 17:27:47 -0700

    Added Car and CarList classes + methods with stubs

---
+++ src/main/model/Car.java
@@ -0,0 +1,35 @@
+package model;
+
+// Creates a new Car object with fields representing the production year, make, model,
+// category, and whether or not it's for sale or not.
+public class Car {
+    private int year;
+    private String make;
+    private String model;
+    private String category;
+    private boolean forSale;
+
+    public Car(int year, String make, String model, String category, boolean forSale) {
+        // STUB
+    }
+
+    public int getYear() {
+        // STUB
+    }
+
+    public String getMake() {
+        // STUB
+    }
+
+    public String getModel() {
+        // STUB
+    }
+
+    public String getCategory() {
+        // STUB
+    }
+
+    public boolean getForSale() {
+        // STUB
+    }
+}
---
+++ src/main/model/CarList.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+public class CarList {
+    private ArrayList<Car> collection;
+
+    public CarList() {
+        // STUB   
+    }
+
+    public void addCar() {
+        // STUB
+    }
+
+    public void removeCar() {
+        // STUB
+    }
+
+    public ArrayList<Car> getCollection() {
+        // STUB
+    }
+}

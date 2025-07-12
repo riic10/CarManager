e5f3988f927aac460e955fed1a6ed7667e4863a3
Eric Ko <koeric@students.cs.ubc.ca>
2025-07-11 22:19:57 -0700

    Added tests for filterCollection method

--- src/test/model/TestCollection.java
+++ src/test/model/TestCollection.java
@@ -13,13 +13,17 @@ public class TestCollection {
     Car testCar1;
     Car testCar2;
     Car testCar3;
+    Car testCar4;
+    Car testCar5;
 
     @BeforeEach
     void runBefore() {
         testCollection = new Collection();
         testCar1 = new Car(2000, "Honda", "Civic", Category.ECONOMY, false);
         testCar2 = new Car(2025, "Porsche", "911 GT3 RS", Category.SUPERCAR, false);
-        testCar3 = new Car(1969, "Dodge", "Charger", Category.SUPERCAR, false);
+        testCar3 = new Car(1969, "Dodge", "Charger", Category.MUSCLE, false);
+        testCar4 = new Car(2020, "Mercedes", "W11", Category.RACECAR, false);
+        testCar5 = new Car(2017, "Bugatti", "Chiron", Category.SUPERCAR, false);
     }
 
     @Test
@@ -58,5 +62,25 @@ public class TestCollection {
         assertEquals(testCar2, testCollection.getCollection().get(1));
         testCollection.removeCar(testCar2);
         assertFalse(testCollection.getCollection().contains(testCar2));
+    }
+
+    @Test
+    void testFilterCollectionOneCar() {
+        testCollection.addCar(testCar1);
+        assertTrue(testCollection.filterCollection(Category.ECONOMY).contains(testCar1));
+    }
+
+    @Test 
+    void testFilterCollectionMultipleCars() {
+        testCollection.addCar(testCar1);
+        testCollection.addCar(testCar2);
+        testCollection.addCar(testCar3);
+        testCollection.addCar(testCar4);
+        testCollection.addCar(testCar5);
+        assertTrue(testCollection.filterCollection(Category.SUPERCAR).contains(testCar2));
+        assertTrue(testCollection.filterCollection(Category.SUPERCAR).contains(testCar5));
+        assertFalse(testCollection.filterCollection(Category.SUPERCAR).contains(testCar1));
+        assertFalse(testCollection.filterCollection(Category.SUPERCAR).contains(testCar3));
+        assertFalse(testCollection.filterCollection(Category.SUPERCAR).contains(testCar4));
     }
 }

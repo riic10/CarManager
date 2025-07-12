0df7dcccc72acd7144a13895ccce27890208e9ac
Eric Ko <koeric@students.cs.ubc.ca>
2025-07-12 13:23:59 -0700

    Added method definitions, specifications, and stubs for CarCollectionManager

--- src/main/ui/CarCollectionManager.java
+++ src/main/ui/CarCollectionManager.java
@@ -18,4 +18,59 @@ public class CarCollectionManager {
         System.out.println("test");
     }
 
+    // MODIFIES: this
+    // EFFECTS: processes user command
+    private void processCommand(String command) {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes collections
+    private void init() {
+        // STUB
+    }
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu() {
+        // STUB
+    }
+
+    // EFFECTS: returns the entire collection
+    private Collection doGetCollection() {
+        return null; // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Adds a new car to the collection
+    private void doAddCar() {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Removes a car from the collection
+    private void doRemoveCar() {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Sets a car in the collection as for sale
+    private void doSetForSale() {
+        // STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Sets a car in the collection as not for sale
+    private void doSetNotForSale() {
+        // STUB
+    }
+
+    // EFFECTS: Returns only the cars that are for sale in the collection
+    private Collection doFilterForSale() {
+        return null; // STUB
+    }
+
+    // EFFECTS: Returns only the cars in the collection which match a given category
+    private Collection doFilterByCollection() {
+        return null; // STUB
+    }
 }

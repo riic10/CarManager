9a39e81d9df9d90cffff06ebb0a4285db39f6b64
Eric Ko <koeric@students.cs.ubc.ca>
2025-07-12 12:26:55 -0700

    Created CarCollectionManager ui class with a test message

---
+++ src/main/ui/CarCollectionManager.java
@@ -0,0 +1,21 @@
+package ui;
+
+import model.*;
+
+import java.util.Scanner;
+
+// Car collection manager app
+public class CarCollectionManager { 
+
+    private Collection collection;
+    private Scanner input;
+
+    public CarCollectionManager() {
+        runManager();    
+    }
+
+    public void runManager() {
+        System.out.println("test");
+    }
+
+}

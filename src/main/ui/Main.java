686da60c5403ecdafc963645d643033edfb9a797
Eric Ko <koeric@students.cs.ubc.ca>
2025-07-14 00:25:22 -0700

    Added welcome message when running the program

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,6 +2,10 @@ package ui;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.println("");
+        System.out.println("--Welcome to the car collection!--");
+        System.out.print("");
+        
         new CarCollectionManager();
     }
 }

dad46b507a9fad3d4e29ae02f7aa5bfba3e4819d
Eric Ko <koeric@students.cs.ubc.ca>
2026-07-28 07:21:01 -0700

    Validate make and model length

--- src/main/java/app/web/dto/CarRequest.java
+++ src/main/java/app/web/dto/CarRequest.java
@@ -4,14 +4,15 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import app.model.Car;
 import app.model.Category;
 
 public record CarRequest(
     @Min(1885) @Max(2100) int year,
-    @NotBlank String make,
-    @NotBlank String model,
+    @NotBlank @Size(max = 100) String make,
+    @NotBlank @Size(max = 100) String model,
     @NotNull Category category,
     boolean forSale) {
 

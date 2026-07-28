f808429ceef2e5e07b6fd29930a3b2b9571fa353
Eric Ko <koeric@students.cs.ubc.ca>
2026-07-28 07:21:02 -0700

    Make service methods transactional

--- src/main/java/app/service/CarService.java
+++ src/main/java/app/service/CarService.java
@@ -3,6 +3,7 @@ package app.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import app.model.Car;
 import app.model.Category;
@@ -16,6 +17,7 @@ public class CarService {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true)
     public Car getById(Long id) {
         return repository.findById(id)
             .orElseThrow(() -> new CarNotFoundException(id));
@@ -25,6 +27,7 @@ public class CarService {
         return repository.save(car);
     }
 
+    @Transactional
     public void deleteById(Long id) {
         if (!repository.existsById(id)) {
             throw new CarNotFoundException(id);
@@ -32,6 +35,7 @@ public class CarService {
         repository.deleteById(id);
     }
 
+    @Transactional(readOnly = true)
     public List<Car> find(Boolean forSale, Category category) {
         if (forSale != null && category != null) {
             return repository.findByForSaleAndCategory(forSale, category);
